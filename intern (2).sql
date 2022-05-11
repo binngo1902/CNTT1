3f841d7c97bba63ef1087ba384a84f9b06382148
Binngo1902 <Binngo1902>
2022-05-11 19:37:14 +0700

    add mysql

--- intern (2).sql
+++ intern (2).sql
@@ -55,7 +55,7 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1);
 
 
 -- --------------------------------------------------------
